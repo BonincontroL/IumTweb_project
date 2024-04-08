131ee25874e53189773a180181cfe1757d950030
Lorenzo Bonincontro <116840033+BonincontroL@users.noreply.github.com>
2024-04-08 10:49:04 +0200

    SpringServer
    Add SpringServer

---
+++ solution/springServer/src/test/java/it/iumtweb/springserver/SpringServerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.iumtweb.springserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
