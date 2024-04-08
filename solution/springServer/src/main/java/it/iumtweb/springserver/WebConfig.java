894cbbb4dd9c368fb8351045c2a1c5efd8acd7ee
Lorenzo Bonincontro <116840033+BonincontroL@users.noreply.github.com>
2024-04-08 11:02:15 +0200

    WebConfig -- CORS

--- solution/springServer/src/main/java/it/iumtweb/springserver/WebConfig.java
+++ solution/springServer/src/main/java/it/iumtweb/springserver/WebConfig.java
@@ -7,6 +7,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    /**
+     * Configura il CORS per consentire le richieste da localhost:3000
+     *
+     */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
