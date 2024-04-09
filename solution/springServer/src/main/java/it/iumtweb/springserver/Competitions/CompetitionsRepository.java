0531a40b78d7d6e7a16130f691451499aed84c52
Lorenzo Bonincontro <116840033+BonincontroL@users.noreply.github.com>
2024-04-09 09:53:48 +0200

    update

--- solution/springServer/src/main/java/it/iumtweb/springserver/Competitions/CompetitionsRepository.java
+++ solution/springServer/src/main/java/it/iumtweb/springserver/Competitions/CompetitionsRepository.java
@@ -1,6 +1,8 @@
 package it.iumtweb.springserver.Competitions;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CompetitionsRepository extends JpaRepository<Competitions, Long> {
 }
