6263171d91f8f6301e213d81907ee32477331a9d
Alberto Aiello <149602899+Albe0218@users.noreply.github.com>
2024-04-12 16:36:03 +0200

    Aggiunta login
    Inizio implementazione login, lato client

---
+++ solution/MainServer/public/javascripts/login.js
@@ -0,0 +1,140 @@
+/**
+ * Inizializza la pagina di login chiamata quando la pagina viene caricata
+ */
+function onLogin(){
+        showLoginForm();
+
+
+}
+
+/**
+ * Aggiunge i listener ai pulsanti del form di login e registrazione
+ */
+function addListenersLogin(){
+    document.getElementById("showSingupPage").addEventListener('click', showRegisterForm);
+
+    document.getElementById('login_button').addEventListener('click', function (event) {
+        event.preventDefault();
+        const requestBody = ExtractDataLog();
+        console.log(requestBody); //da eliminare solo per vedere se stampa i dati
+        if (requestBody !== null) {
+
+        }
+        resetLoginForm();
+    });
+
+}
+
+/**
+ * aggiunge i listener ai pulsanti del form di registrazione
+ */
+function addListenersRegister(){
+    document.getElementById("showLoginPage").addEventListener('click', showLoginForm);
+
+    document.getElementById('singUp_button').addEventListener('click', function(event) {
+        event.preventDefault();
+        const requestBody = ExtractDataReg();
+        console.log(requestBody); // da eliminare oslo per vedere se stampa
+        if (requestBody !== null) {
+
+        }
+        resetRegisterForm();
+    });
+
+
+}
+
+/**
+ * mostra il form di login e nasconde il form di registrazione
+ */
+function showLoginForm() {
+    document.getElementById("singup_form").style.display = "none";
+    document.getElementById("login_form").style.display = "flex";
+    addListenersLogin();
+}
+
+/**
+ * mostra il form di registrazione e nasconde il form di login
+ */
+function showRegisterForm(){
+    document.getElementById("singup_form").style.display = "flex";
+    document.getElementById("login_form").style.display = "none";
+    addListenersRegister();
+}
+
+
+
+/**
+ * Estrae i dati dal form di login
+ * @returns requestBody struttura con i dati del form se son validi , se no ritorna null;
+ * @constructor
+ */
+function ExtractDataLog() {
+    const formData = new FormData(document.getElementById('login_form'));
+    const requestBody = Object.fromEntries(formData.entries());
+
+    const email = requestBody['email'];
+    if (!validateEmail(email)) {
+        alert('Indirizzo email non valido');
+        return null;
+    }
+    const password = requestBody['password'];
+    if (!password) {
+        alert('Password non può essere vuota');
+        return null;
+    }
+    return requestBody;
+}
+
+
+/**
+ * Estrae i dati dal form di registrazione
+ * @returns requestBody struttura con i dati del form se son validi , se no ritorna null
+ * @constructor
+ */
+function ExtractDataReg() {
+    const formData = new FormData(document.getElementById('singup_form'));
+    const requestBody = Object.fromEntries(formData.entries()); //trasforma i dati del form in un oggetto
+
+    const email = requestBody['email'];
+    if (!validateEmail(email)) {
+        alert('Indirizzo email non valido');
+        return null;
+    }
+
+    const username = requestBody['username'];
+    const password = requestBody['password'];
+    if (!username || !password) {
+        alert('Username e/o password non possono essere vuoti');
+        return null;
+    }
+
+    return requestBody;
+}
+
+/**
+ * Pulisce il form di registrazione
+
+ */
+function resetRegisterForm() {
+    document.getElementById('singup_form').reset();
+}
+
+/**
+ * Resetta il form di login
+ */
+function resetLoginForm() {
+    document.getElementById('login_form').reset();
+}
+
+
+
+/**
+ * regex per la validazione dell'email
+ * @param email estratta dal form
+ * @returns {boolean} true se l'email è valida, false altrimenti
+ */
+function validateEmail(email) {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(String(email).toLowerCase());
+}
