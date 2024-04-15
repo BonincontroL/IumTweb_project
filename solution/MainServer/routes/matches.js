6f76bb129db0aa5c5084c5bbe9c821ead64d2b4b
Lorenzo Bonincontro <116840033+BonincontroL@users.noreply.github.com>
2024-04-15 19:38:36 +0200

    update + creazione router matches.js

---
+++ solution/MainServer/routes/matches.js
@@ -0,0 +1,17 @@
+var express = require('express');
+let axios=require('axios')
+var router = express.Router();
+const EXPRESS_SERVER="http://localhost:3001";
+const SPRING_SERVER="http://localhost:8081";
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
