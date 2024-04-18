f55ae8d221c40e7ed8910f5452451c0d27548a3b
Alberto Aiello <149602899+Albe0218@users.noreply.github.com>
2024-04-18 11:41:42 +0200

    Aggiunta routes per statistiche giocatore
    aggiunta su Express per cartellini , minuti giocati

--- solution/ExpressServer/controllers/appearances.js
+++ solution/ExpressServer/controllers/appearances.js
@@ -20,6 +20,47 @@ function getTopScorer(competitionId){
         }
     ])
 }
+
+function getPlayerStatisticsFor2023(playerId) {
+    return Model.aggregate([
+        {
+            $match: {
+                player_id: playerId, // Filtro per player_id
+                date: {
+                    $gte: new Date('2023-01-01T00:00:00Z'),
+                    $lte: new Date('2023-12-31T23:59:59Z')
+                }
+            }
+        },
+        {
+            $group: {
+                _id: '$player_id',
+                total_goals: { $sum: '$goals' },
+                total_assists: { $sum: '$assists' },
+                total_yellow_cards: { $sum: '$yellow_cards' },
+                total_red_cards: { $sum: '$red_cards' },
+                appearances: { $sum: 1 },
+                player_name: { $first: '$player_name' }
+            }
+        },
+        {
+            $project: {
+                _id: 0,
+                player_id: '$_id',
+                player_name: 1,
+                total_goals: 1,
+                total_assists: 1,
+                total_yellow_cards: 1,
+                total_red_cards: 1,
+                appearances: 1
+            }
+        }
+    ]);
+}
+
+
+
 module.exports={
-    getTopScorer
+    getTopScorer,
+    getPlayerStatisticsFor2023
 }
--- solution/ExpressServer/routes/appearances.js
+++ solution/ExpressServer/routes/appearances.js
@@ -16,4 +16,21 @@ router.get('/getTopScorer', async (req,res)=>{
             res.status(500).json({ error: 'Errore durante il recupero dei top scorer' });
         });
 })
+
+router.get(`/getPlayerStatisticsFor2023/:playerId`, async (req, res)=>{
+    appearancesController.getPlayerStatisticsFor2023(req.params.playerId)
+        .then(data=>{
+            res.status(200).json(data);
+        })
+        .catch(err=>{
+            console.error('Errore durante il recupero delle statistiche del giocatore', err);
+            res.status(500).json({ error: 'Errore durante il recupero delle statistiche del giocatore' });
+        });
+})
+
+
+
+
+
+
 module.exports=router
