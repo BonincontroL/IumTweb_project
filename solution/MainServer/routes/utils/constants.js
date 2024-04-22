757a9bd470c6b40e95c4e642847a5ab04998055e
Lorenzo Bonincontro <bonincontrolorenzo@gmail.com>
2024-04-22 21:02:50 +0200

    aggiornamento + aggiunta file in router per costanti constants.js

---
+++ solution/MainServer/routes/utils/constants.js
@@ -0,0 +1,11 @@
+//FILE var COSTANTI
+
+const EXPRESS_SERVER="http://localhost:3001"
+const SPRING_SERVER="http://localhost:8081"
+
+
+
+module.exports = {
+    EXPRESS_SERVER,
+    SPRING_SERVER
+};
